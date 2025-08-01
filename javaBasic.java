177e68b5d1aafce5f908a93e5cc67e60717d293a
harshh3400 <plshinde98@gmail.com>
2025-08-01 20:05:11 +0530

    Add files via upload

---
+++ javaBasic.java
@@ -0,0 +1,10 @@
+public class javaBasic {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        for(int i=0;i<=5;i++)
+        {
+            for(int j=5;j>=0;j--)
+            System.out.println("*");
+        }
+    }
+}
